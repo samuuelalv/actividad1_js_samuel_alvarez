cc071c373cd0aaede138dc41901c2db6288de42e
samuel alvarez <105326071+samuuelalv@users.noreply.github.com>
2022-07-23 16:05:05 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let name = 'samuel', 
+lastname = 'alvarez', 
+yearOfBirth = 2005, 
+cityOfBirth = 'medellin'
+
+console.log(
+    `Hola, mi nombre es ${name} ${lastname}, vivo en ${cityOfBirth} y tengo ${2022 - yearOfBirth} años.`
+)
